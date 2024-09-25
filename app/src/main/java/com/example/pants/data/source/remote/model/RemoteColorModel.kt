ab0ce8b24d73b39bfdaa7af2c75c2931411b4cac
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2024-09-26 01:28:25 +0300

    PANTS-0 merged PANTS-4-refactoring-getRandomColors-function into PANTS-0

--- app/src/main/java/com/example/pants/data/source/remote/model/RemoteColorModel.kt
+++ app/src/main/java/com/example/pants/data/source/remote/model/RemoteColorModel.kt
@@ -1,12 +1,14 @@
 package com.example.pants.data.source.remote.model
 
+import java.io.Serializable
+
 data class ColorResponse(
     val name: Name,
     val hsv: Hsv
-)
+) : Serializable
 
-data class Name(val value: String)
+data class Name(val value: String): Serializable
 
-data class Hsv(val fraction: Fraction) {
-    data class Fraction(val h: Float, val s: Float, val v: Float)
+data class Hsv(val fraction: Fraction) : Serializable {
+    data class Fraction(val h: Float, val s: Float, val v: Float) : Serializable
 }
